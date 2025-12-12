7e84583cc2ff5b244c8a047d416e02ac932464bc
dhruv <pateldhruv2723@gmail.com>
2025-12-13 01:50:13 +0530

    Fix CORS and Gmail password

--- foodproject/settings.py
+++ foodproject/settings.py
@@ -103,8 +103,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='pateldhruv8404@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='chqm yfro jxnp wtid')
+
+# USE YOUR APP PASSWORD HERE (NO SPACES)
+EMAIL_HOST_USER = 'pateldhruv8404@gmail.com'
+EMAIL_HOST_PASSWORD = 'chqmyfrojxnpwtid'  # → REMOVE ALL SPACES
 
 # 🔥 CORS SETTINGS (FINAL CLEAN VERSION)
 CORS_ALLOW_ALL_ORIGINS = False
